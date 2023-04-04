490e51b9a967c92f0ebc8c7f7034ba92176458c2
KenChang <yunyi2525@qq.com>
2023-04-04 22:04:04 +0800

    config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = 'v1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_parser",'recommonmark','sphinx_markdown_tables']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,5 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
